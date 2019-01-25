fdfbec769d345381971729435874936661c19634
ikura1 <ikura.lab@gmail.com>
2019-01-26 01:31:21 +0900

    refactor: folder内に移動してみる

---
+++ money/test_money.py
@@ -0,0 +1,13 @@
+from money.dollar import Dollar
+import unittest
+
+
+class MoneyTest(unittest.TestCase):
+    def test_multiplication(self):
+        five = Dollar(5)
+        five.times(2)
+        self.assertEqual(10, five.amount)
+
+
+if __name__ == "__main__":
+    unittest.main()
