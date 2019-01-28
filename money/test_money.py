f5667851ed16319625568de515433e90cfbf6c65
ikura1 <ikura.lab@gmail.com>
2019-01-28 23:51:04 +0900

    test: objectを返す形にしてみる

--- money/test_money.py
+++ money/test_money.py
@@ -5,10 +5,10 @@ import unittest
 class MoneyTest(unittest.TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
-        five.times(3)
-        self.assertEqual(15, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
 
 
 if __name__ == "__main__":
