ae717df175725712c5ad51816818b258485a0807
ikura1 <ikura.lab@gmail.com>
2019-01-28 23:54:24 +0900

    refactor: Dollarを返すように変更

--- money/dollar.py
+++ money/dollar.py
@@ -9,5 +9,4 @@ class Dollar:
         self.amount = amount
 
     def times(self, multiplier):
-        self.amount *= multiplier
-        return None
+        return Dollar(self.amount * multiplier)
