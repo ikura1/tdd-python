d875015c6461c0eb3c852409f81b1e48ee057870
ikura1 <ikura.lab@gmail.com>
2019-01-29 00:09:30 +0900

    test: add equals test

--- money/test_money.py
+++ money/test_money.py
@@ -10,6 +10,9 @@ class MoneyTest(unittest.TestCase):
         product = five.times(3)
         self.assertEqual(15, product.amount)
 
+    def test_equality(self):
+        self.assertTrue(Dollar(5).equals(Dollar(5)))
+
 
 if __name__ == "__main__":
     unittest.main()
