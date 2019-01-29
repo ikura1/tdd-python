e15ac4bf9e1dd9ce31868cb0299895bf705b8974
ikura1 <ikura.lab@gmail.com>
2019-01-30 00:39:18 +0900

    refactor: amountのgetterを実装(pythonのprivateの使い方がわからない)

--- money/dollar.py
+++ money/dollar.py
@@ -10,8 +10,12 @@ class Dollar:
     def __init__(self, amount):
         self.__amount = amount
 
+    @property
+    def amount(self):
+        return self.__amount
+
     def times(self, multiplier):
         return Dollar(self.__amount * multiplier)
 
     def equals(self, dollar):
-        return self.__amount == dollar._Dollar__amount
+        return self.__amount == dollar.amount
