007a0a059eb02aba3857fdc30db4e64dcc81a97d
ikura1 <ikura.lab@gmail.com>
2019-01-31 00:55:03 +0900

    refactor: __eq__を親に移動しているため削除(型判定の下りはまだ自分には早い)

--- moneys/franc.py
+++ moneys/franc.py
@@ -4,6 +4,3 @@ from moneys.money import Money
 class Franc(Money):
     def times(self, multiplier):
         return Franc(self.amount * multiplier)
-
-    def __eq__(self, franc):
-        return self.amount == franc.amount
