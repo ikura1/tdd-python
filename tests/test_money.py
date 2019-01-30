342086641b8d0aafdeeda9977f9838ca6f7aa43b
ikura1 <ikura.lab@gmail.com>
2019-01-31 00:51:01 +0900

    test: add Francの等価テスト

--- tests/test_money.py
+++ tests/test_money.py
@@ -12,6 +12,8 @@ class MoneyTest(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Dollar(5).__eq__(Dollar(5)))
         self.assertFalse(Dollar(5).__eq__(Dollar(6)))
+        self.assertTrue(Franc(5).__eq__(Franc(5)))
+        self.assertFalse(Franc(5).__eq__(Franc(6)))
 
     def test_franc_multiplication(self):
         five = Franc(5)
