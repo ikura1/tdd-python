01b0ff209fbe31d517edcde95d70a7d37d090ee0
ikura1 <ikura.lab@gmail.com>
2019-02-01 03:02:09 +0900

    test: Francも同様にDollarから呼ぶ形に変更

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,3 @@
-from moneys.franc import Franc
 from moneys.money import Money
 import unittest
 
@@ -12,14 +11,14 @@ class MoneyTest(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Money.dollar(5).__eq__(Money.dollar(5)))
         self.assertFalse(Money.dollar(5).__eq__(Money.dollar(6)))
-        self.assertTrue(Franc(5).__eq__(Franc(5)))
-        self.assertFalse(Franc(5).__eq__(Franc(6)))
-        self.assertFalse(Franc(5).__eq__(Money.dollar(5)))
+        self.assertTrue(Money.franc(5).__eq__(Money.franc(5)))
+        self.assertFalse(Money.franc(5).__eq__(Money.franc(6)))
+        self.assertFalse(Money.franc(5).__eq__(Money.dollar(5)))
 
     def test_franc_multiplication(self):
-        five = Franc(5)
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
 
 if __name__ == "__main__":
