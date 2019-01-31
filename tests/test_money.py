95c24fac29c593c515ea98a0be44c15e29570434
ikura1 <ikura.lab@gmail.com>
2019-02-01 02:59:55 +0900

    test: Dollarを全てMoneyから生成する形に変更

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,3 @@
-from moneys.dollar import Dollar
 from moneys.franc import Franc
 from moneys.money import Money
 import unittest
@@ -7,15 +6,15 @@ import unittest
 class MoneyTest(unittest.TestCase):
     def test_multiplication(self):
         five = Money.dollar(5)
-        self.assertEqual(Dollar(10), five.times(2))
-        self.assertEqual(Dollar(15), five.times(3))
+        self.assertEqual(Money.dollar(10), five.times(2))
+        self.assertEqual(Money.dollar(15), five.times(3))
 
     def test_equality(self):
-        self.assertTrue(Dollar(5).__eq__(Dollar(5)))
-        self.assertFalse(Dollar(5).__eq__(Dollar(6)))
+        self.assertTrue(Money.dollar(5).__eq__(Money.dollar(5)))
+        self.assertFalse(Money.dollar(5).__eq__(Money.dollar(6)))
         self.assertTrue(Franc(5).__eq__(Franc(5)))
         self.assertFalse(Franc(5).__eq__(Franc(6)))
-        self.assertFalse(Franc(5).__eq__(Dollar(5)))
+        self.assertFalse(Franc(5).__eq__(Money.dollar(5)))
 
     def test_franc_multiplication(self):
         five = Franc(5)
