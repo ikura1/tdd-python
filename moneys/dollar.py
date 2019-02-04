fd8b270ba83f280f94869da7de9551b80b80f807
ikura1 <ikura.lab@gmail.com>
2019-02-05 00:20:05 +0900

    doc: edit TODO

--- moneys/dollar.py
+++ moneys/dollar.py
@@ -4,8 +4,6 @@
 # TODO: nullとの等価性比較
 # TODO: 他のオブジェクトとの等価性比較
 # TODO: DollarとFrancの重複
-# TODO: timesの一般化
-# TODO: Franc とDollarを比較する
 # TODO: testFrancMultiplicationを削除する？
 from moneys.money import Money
 
