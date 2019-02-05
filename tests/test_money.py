b37b45c1618b1053d67e6026eaa5665a502a954b
ikura1 <ikura.lab@gmail.com>
2019-02-06 00:00:01 +0900

    test: サブクラス削除による重複テストを削除

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,5 +1,4 @@
 from moneys.money import Money
-from moneys.franc import Franc
 import unittest
 
 
@@ -12,21 +11,11 @@ class MoneyTest(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Money.dollar(5).__eq__(Money.dollar(5)))
         self.assertFalse(Money.dollar(5).__eq__(Money.dollar(6)))
-        self.assertTrue(Money.franc(5).__eq__(Money.franc(5)))
-        self.assertFalse(Money.franc(5).__eq__(Money.franc(6)))
         self.assertFalse(Money.franc(5).__eq__(Money.dollar(5)))
-
-    def test_franc_multiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(10), five.times(2))
-        self.assertEqual(Money.franc(15), five.times(3))
 
     def test_currency(self):
         self.assertEqual("USD", Money.dollar(1).currency())
         self.assertEqual("CHF", Money.franc(1).currency())
-
-    def test_different_class_equality(self):
-        self.assertTrue(Money(10, "CHF").__eq__(Franc(10, "CHF")))
 
 
 if __name__ == "__main__":
