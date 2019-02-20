8032ea440d146f7600ace9fb49b9ec427d8a5318
ikura1 <ikura.lab@gmail.com>
2019-02-21 01:06:21 +0900

    feat: 19章 setupを実装した

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -12,17 +16,25 @@ class WasRun(TestCase):
         self.wasRun = None
         super().__init__(name)
 
+    def setUp(self):
+        self.wasSetUp = 1
+
     def testMethod(self):
         self.wasRun = 1
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("testRunning").run()
-
+TestCaseTest("testSetUp").run()
