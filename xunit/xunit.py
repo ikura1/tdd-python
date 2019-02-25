70687c08ff8ba9029b6bf0470eef3f321095cf45
ikura1 <ikura.lab@gmail.com>
2019-02-26 00:52:30 +0900

    feat: testSuiteを実装してく

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -72,8 +72,17 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert "1 run, 1 failed" == result.summary()
 
+    def testSuiter(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = suite.run()
+        assert "2 run, 1 failed" == result.summary()
+
 
 print(TestCaseTest("testTemplateMethod").run().summary())
 print(TestCaseTest("testResult").run().summary())
 print(TestCaseTest("testFailedResult").run().summary())
 print(TestCaseTest("testFailedResultFormatting").run().summary())
+print(TestCaseTest("testSuite").run().summary())
+
